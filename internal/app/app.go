ac7f154893f33c2878785d8b9a5f9de3587e58c0
Sim Chan <raindow03@gmail.com>
2026-01-05 04:07:05 +0400

    feat: Add grpc handler for user. Create, Update, SelectById

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,16 +2,39 @@ package app
 
 import (
 	"bill-split/internal/config"
-	"bill-split/internal/handler"
+	"bill-split/internal/domain/service"
+	"bill-split/internal/repository"
+	proto "bill-split/proto/this"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 )
 
 func Start() error {
-	dbpool := config.NewInterfaces()
+	dbpool := config.NewInterfaces(config.InitDb())
 
 	defer dbpool.DbClose()
 
-	handlers := handler.NewHandlers(dbpool)
-	handlers.InitRoutes()
+	userRepo := repository.NewUserRepository(dbpool.GetSqlxDb())
+	userService := service.NewUserService(userRepo)
+
+	//handlers := handler.NewHandlers(dbpool)
+	//handlers.InitRoutes()
+
+	grpcServer := grpc.NewServer()
+
+	proto.RegisterUserServiceServer(grpcServer, userService)
+
+	// Запускаем на порту 30000
+	lis, err := net.Listen("tcp", ":30000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("gRPC Server started on port 30000")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatal(err)
+	}
 
 	return nil
 }
--- internal/repository/user.go
+++ internal/repository/user.go
@@ -1,1 +1,67 @@
 package repository
+
+import (
+	"bill-split/internal/domain/entity/user"
+	"github.com/jmoiron/sqlx"
+)
+
+type UserRepository interface {
+	GetUserById(id int) (*user.User, error)
+	CreateUser(userData user.User) (int64, error)
+	UpdateUser(userData user.User) error
+}
+
+type userRepository struct {
+	db *sqlx.DB
+}
+
+func NewUserRepository(db *sqlx.DB) UserRepository {
+	return &userRepository{
+		db: db,
+	}
+}
+
+func (u *userRepository) CreateUser(userData user.User) (int64, error) {
+	err := u.db.QueryRow(
+		`INSERT INTO "user" (name, email, phone, login, password) 
+						VALUES ($1, $2, $3, $4, $5) RETURNING id;`,
+		userData.Name, userData.Email, userData.Phone, userData.Login, userData.Password,
+	).Scan(&userData.Id)
+	if err != nil {
+		return 0, err
+	}
+
+	return userData.Id, nil
+}
+
+func (u *userRepository) UpdateUser(userData user.User) error {
+	_, err := u.db.Exec(`
+		UPDATE "user"
+		SET name = $1, email = $2, phone = $3, password = $4 
+		WHERE id = $5
+		`,
+		userData.Name, userData.Email, userData.Phone, userData.Password, &userData.Id,
+	)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (u *userRepository) GetUserById(id int) (*user.User, error) {
+	var user user.User
+
+	err := u.db.QueryRow(`
+		SELECT name, email, phone, login
+		FROM "user"
+		WHERE id = $1
+	`, id,
+	).Scan(
+		&user.Name, &user.Email, &user.Phone, &user.Login,
+	)
+	if err != nil {
+		return nil, err
+	}
+	return &user, nil
+}
