dfdf0921bc1dd44826de66e313c2ffb66d780072
Sim <raindow03@gmail.com>
2026-01-15 16:58:41 +0300

    refactor: Change the driver from sqlx.DB to pgxpool.Pool

--- internal/config/postgres.go
+++ internal/config/postgres.go
@@ -5,38 +5,81 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"runtime"
 	"time"
 
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
-	"github.com/jackc/pgx/v5/stdlib"
-	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 )
 
 type Postgres interface {
-	GetSqlxDb() *sqlx.DB
-	DbClose() error
+	NewPoolConfig(maxConn int, connIdleTime, connLifeTime time.Duration) error
+	GetSql() *pgxpool.Pool
+	DbClose()
 }
 
 type postgres struct {
-	Dbpg *sqlx.DB
+	conn         *pgxpool.Pool
+	connStr      string
+	poolConfig   *pgxpool.Config
+	queryTimeout time.Duration
 }
 
-func NewInterfaces(db *sqlx.DB) Postgres {
+func New(db *pgxpool.Pool, connStr string) Postgres {
 	return &postgres{
-		Dbpg: db,
+		conn:    db,
+		connStr: connStr,
 	}
 }
 
-func (i *postgres) DbClose() error {
-	return i.Dbpg.Close()
+func (p *postgres) NewPoolConfig(maxConn int, connIdleTime, connLifeTime time.Duration) error {
+	poolConfig, err := pgxpool.ParseConfig(p.connStr)
+	if err != nil {
+		return err
+	}
+
+	// Смотрим, чтобы соединений к бд было меньше чем ядер
+	cpu := runtime.NumCPU()
+	if maxConn < cpu {
+		maxConn = cpu
+	}
+
+	poolConfig.MaxConns = int32(maxConn)
+	poolConfig.MaxConnIdleTime = connIdleTime
+	poolConfig.MaxConnLifetime = connLifeTime
+	p.poolConfig = poolConfig
+	return nil
 }
 
-func (i *postgres) GetSqlxDb() *sqlx.DB {
-	return i.Dbpg
+func (p *postgres) DbClose() {
+	p.conn.Close()
 }
 
-func InitDb() *sqlx.DB {
+func (p *postgres) GetSql() *pgxpool.Pool {
+	return p.conn
+}
+
+func (p *postgres) Ping(ctx context.Context) error {
+	return p.conn.Ping(ctx)
+}
+
+func (p *postgres) Exec(ctx context.Context, sql string, arguments ...any) (pgconn.CommandTag, error) {
+	return p.conn.Exec(ctx, sql, arguments...)
+}
+
+func (p *postgres) Query(ctx context.Context, sql string, arguments ...any) (pgx.Rows, error) {
+	return p.conn.Query(ctx, sql, arguments...)
+}
+
+func (p *postgres) QueryRow(ctxParent context.Context, sql string, arguments ...any) pgx.Row {
+	ctx, cancel := context.WithTimeout(ctxParent, p.queryTimeout)
+	defer cancel()
+	return p.conn.QueryRow(ctx, sql, arguments...)
+}
+
+func InitDb() (*pgxpool.Pool, string) {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
@@ -62,11 +105,7 @@ func InitDb() *sqlx.DB {
 		log.Printf("Не удалось подключиться к базе данных: %v\n", err)
 	}
 
-	//Конвертируем в тип *sqlx.DB
-	sqlDB := stdlib.OpenDBFromPool(dbpool)
-	db := sqlx.NewDb(sqlDB, "postgres")
-
 	log.Println("Подключение к базе данных прошла успешно.")
 
-	return db
+	return dbpool, connStr
 }
--- internal/repository/user.go
+++ internal/repository/user.go
@@ -2,7 +2,10 @@ package repository
 
 import (
 	"bill-split/internal/domain/entity/user"
-	"github.com/jmoiron/sqlx"
+	"context"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type UserRepository interface {
@@ -12,17 +15,21 @@ type UserRepository interface {
 }
 
 type userRepository struct {
-	db *sqlx.DB
+	db *pgxpool.Pool
 }
 
-func NewUserRepository(db *sqlx.DB) UserRepository {
+func NewUserRepository(db *pgxpool.Pool) UserRepository {
 	return &userRepository{
 		db: db,
 	}
 }
 
 func (u *userRepository) CreateUser(userData user.User) (int64, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*15))
+	defer cancel()
+
 	err := u.db.QueryRow(
+		ctx,
 		`INSERT INTO "user" (name, email, phone, login, password) 
 						VALUES ($1, $2, $3, $4, $5) RETURNING id;`,
 		userData.Name, userData.Email, userData.Phone, userData.Login, userData.Password,
@@ -35,7 +42,12 @@ func (u *userRepository) CreateUser(userData user.User) (int64, error) {
 }
 
 func (u *userRepository) UpdateUser(userData user.User) error {
-	_, err := u.db.Exec(`
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*15))
+	defer cancel()
+
+	_, err := u.db.Exec(
+		ctx,
+		`
 		UPDATE "user"
 		SET name = $1, email = $2, phone = $3, password = $4 
 		WHERE id = $5
@@ -50,9 +62,14 @@ func (u *userRepository) UpdateUser(userData user.User) error {
 }
 
 func (u *userRepository) GetUserById(id int) (*user.User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(time.Second*15))
+	defer cancel()
+
 	var user user.User
 
-	err := u.db.QueryRow(`
+	err := u.db.QueryRow(
+		ctx,
+		`
 		SELECT name, email, phone, login
 		FROM "user"
 		WHERE id = $1
