3b9d410c8a6a491c88c9e417be32adaed029d799
ChanKachan <raindow03@gmail.com>
2026-02-14 15:20:24 +0400

    feate/add optimizationService

--- internal/domain/entity/transfer/transfer.go
+++ internal/domain/entity/transfer/transfer.go
@@ -1,7 +1,16 @@
 package transfer
 
-type Transfer struct {
-	From   string  `json:"from"`
-	To     string  `json:"to"`
-	Amount float32 `json:"amount"`
+// Участник представляет пользователя с его итоговым балансом.
+// Отрицательный баланс означает, что участник должен деньги (должник).
+// Положительный баланс означает, что участнику должны деньги (получатель).
+type Participant struct {
+	Name    string
+	Balance float64
+}
+
+// Transaction представляет собой рекомендуемую операцию перевода.
+type Transaction struct {
+	From   string
+	To     string
+	Amount float64
 }
--- internal/domain/service/authService.go
+++ internal/domain/service/authService.go
@@ -1,10 +1,30 @@
 package service
 
+import (
+	"bill-split/internal/domain/entity/auth"
+	"bill-split/internal/repository"
+	"github.com/gin-gonic/gin"
+)
+
 type AuthService interface {
 }
 
-type authService struct{}
+type authService struct {
+	user repository.UserRepository
+}
 
-func NewAuthService() AuthService {
-	return &authService{}
+func NewAuthService(user repository.UserRepository) AuthService {
+	return &authService{
+		user: user,
+	}
+}
+
+func (service *authService) Auth(c *gin.Context) {
+	var authData auth.Auth
+	if err := c.ShouldBind(authData); err != nil {
+		c.JSON(400, gin.H{
+			"error": err,
+		})
+	}
+
 }
